de6b94964fe7f5ede004628274ae1a3ea1e80a51
Jakob Renpening <jakob@claimsoluciones.com>
2012-08-22 20:38:10 +0200

    Initial Commit

---
+++ WalterJakobMobile/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  WalterJakobMobile
+//
+//  Created by Jakob Hans Renpening on 22/8/12.
+//  Copyright (c) 2012 WalterJakob. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
---
+++ WalterJakobMobile/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WalterJakobMobile
+//
+//  Created by Jakob Hans Renpening on 22/8/12.
+//  Copyright (c) 2012 WalterJakob. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
